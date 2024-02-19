312e48eaf270a6dbfdb9ca1ea7dd05faca2fcfc6
Jesse-js <jesse-jjs@hotmail.com>
2024-02-19 17:46:37 -0300

    feat: add code setup for flexbox layout

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import Box from './components/Box';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Box style={{ backgroundColor: 'red' }}>Box 1</Box>
+      <Box style={{ backgroundColor: 'green' }}>Box 2</Box>
+      <Box style={{ backgroundColor: 'purple' }}>Box 3</Box>
+      <Box style={{ backgroundColor: 'orange' }}>Box 4</Box>
+      <Box style={{ backgroundColor: 'blue' }}>Box 5</Box>
+      <Box style={{ backgroundColor: 'yellow' }}>Box 6</Box>
+      <Box style={{ backgroundColor: 'pink' }}>Box 7</Box>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+   marginTop: 20,
+   borderWidth: 6,
+   borderColor: 'black',
   },
 });
