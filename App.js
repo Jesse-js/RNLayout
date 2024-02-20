eeb246cf1b84215831ce21d4598e9352b08ab026
Jesse-js <jesse-jjs@hotmail.com>
2024-02-20 09:55:10 -0300

    feat: working with flexDirection

--- App.js
+++ App.js
@@ -6,13 +6,13 @@ export default function App() {
 
     // </View>
     <View style={styles.container}>
-      <Box style={{ backgroundColor: 'silver', flex: 1 }}>Box 1</Box>
-      <Box style={{ backgroundColor: 'green', flex: 1 }}>Box 2</Box>
-      <Box style={{ backgroundColor: 'purple', flex: 1 }}>Box 3</Box>
-      <Box style={{ backgroundColor: 'orange' }}>Box 4</Box>
+      <Box style={{ backgroundColor: 'silver', flex: 0 }}>Box 1</Box>
+      <Box style={{ backgroundColor: 'green', flex: 0 }}>Box 2</Box>
+      <Box style={{ backgroundColor: 'purple', flex: 0 }}>Box 3</Box>
+      {/* <Box style={{ backgroundColor: 'orange' }}>Box 4</Box>
       <Box style={{ backgroundColor: 'blue' }}>Box 5</Box>
       <Box style={{ backgroundColor: 'yellow' }}>Box 6</Box>
-      <Box style={{ backgroundColor: 'pink' }}>Box 7</Box>
+      <Box style={{ backgroundColor: 'pink' }}>Box 7</Box> */}
     </View>
   );
 }
@@ -20,6 +20,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
    flex: 1,
+   // set the direction of the main axis
+   flexDirection: 'row-reverse', //column, column-reverse, row, row-reverse 
    marginTop: 20,
    borderWidth: 6,
    borderColor: 'red',
