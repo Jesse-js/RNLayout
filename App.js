6ec67a859c51e75bd79d85a099dc9694d3947601
Jesse-js <jesse-jjs@hotmail.com>
2024-02-27 14:22:48 -0300

    feat: working with flexGrow

--- App.js
+++ App.js
@@ -3,13 +3,13 @@ import Box from "./components/Box";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Box style={{ backgroundColor: "silver", flexShrink: 1  }}>Box 1 shrink</Box>
-      <Box style={{ backgroundColor: "green", flexShrink: 1  }}>Box 2 shrink</Box>
-      {/* <Box style={{ backgroundColor: "purple" }}>Box 3</Box>
+      <Box style={{ backgroundColor: "silver" }}>Box 1</Box>
+      <Box style={{ backgroundColor: "green" }}>Box 2</Box>
+      <Box style={{ backgroundColor: "purple" }}>Box 3</Box>
       <Box style={{ backgroundColor: "orange" }}>Box 4</Box>
       <Box style={{ backgroundColor: "blue" }}>Box 5</Box>
       <Box style={{ backgroundColor: "yellow" }}>Box 6</Box>
-      <Box style={{ backgroundColor: "pink" }}>Box 7</Box> */}
+      <Box style={{ backgroundColor: "pink" }}>Box 7</Box>
     </View>
   );
 }
@@ -17,11 +17,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'row',
-    alignItems: 'flex-start',
-    width: 300,
+    flexDirection: "column",
+    alignItems: "stretch",
     marginTop: 64,
     borderWidth: 6,
-    borderColor: 'red',
+    borderColor: "red",
   },
 });
